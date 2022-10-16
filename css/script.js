2b7e3385e2036f41d73aded74b8d4f2928381a41
sekebaev <104671396+sekebaev@users.noreply.github.com>
2022-10-16 15:32:53 +0600

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,9 @@
+var click_kol = false;
+
+function myFunction(){
+    click_kol = true
+    document.getElementById("b").style.position = "relative";
+    document.getElementById("b").style.top = "50px";
+    document.getElementById("b").style.left = "65px";
+    document.getElementById("bars").style.display = "block";
+}
